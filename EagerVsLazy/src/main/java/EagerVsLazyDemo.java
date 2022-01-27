917a430789d22517dd5f1cba4e342d342a52a8e3
shawn <stevenjee9@gmail.com>
2022-01-27 14:31:53 -0500

    learn eager and lazy

---
+++ EagerVsLazy/src/main/java/EagerVsLazyDemo.java
@@ -0,0 +1,49 @@
+import entity.Course;
+import entity.Instructor;
+import entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author Stevenjee
+ */
+public class EagerVsLazyDemo {
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            currentSession.beginTransaction();
+
+            // get the instructor from db
+            int theId = 4;
+            Instructor tempInstructor = currentSession.get(Instructor.class, theId);
+
+            // get courses from the instructor
+            System.out.println("Debug: Instructor: " + tempInstructor);
+            System.out.println("Debug: Courses: " + tempInstructor.getCourses());
+
+            // commit the transaction
+            currentSession.getTransaction().commit();
+
+            System.out.println("Debug: Done!");
+
+        } finally {
+            currentSession.close();
+            sessionFactory.close();
+        }
+
+
+    }
+
+}
